750dbb126c3b8b5bafb49659d5519a6486ea2cd4
Carrie Wright <cwrigh60@jhu.edu>
2020-05-22 13:29:52 -0400

    rearranging layout

--- ui.R
+++ ui.R
@@ -16,25 +16,23 @@ fluidPage(
   #theme = shinytheme("cerulean"),
   theme = shinytheme("darkly"),
   headerPanel("Create a Tutorial"),
-  title = 'Download a PDF report',
   sidebarLayout(
     sidebarPanel(
       helpText(),
-      fileInput("file", "Choose files", multiple = TRUE),
       textInput("logo", "Logo", "Logo_URL"),
       textInput("title", "Title", "Type Title text here"),
-      textInput("header1", "Header1", "test"),
-      selectInput('x', 'Build a regression model of Body weight against:',
-                  choices = names(mammals)[-2]),
       radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
                    inline = TRUE),
-      textInput("narrative1", "Narrative Text", "Type narrative text here"),
-      verbatimTextOutput("value"),
       downloadButton('downloadReport')
     ),
     mainPanel(
-      plotOutput('regPlot')
-
+      plotOutput('regPlot'),
+      fileInput("image", "Choose image files", multiple = TRUE),
+      textInput("header1", "Header1", "test"),
+      selectInput('x', 'Build a regression model of Body weight against:',
+                  choices = names(mammals)[-2]),
+      textInput("narrative1", "Narrative Text", "Type narrative text here"),
+      verbatimTextOutput("value")
     )
   )
 
